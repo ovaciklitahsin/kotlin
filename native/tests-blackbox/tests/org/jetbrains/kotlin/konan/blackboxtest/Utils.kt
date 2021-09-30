168f6021f4d2d55b236bba1e68bbbfa316cdb173
Dmitriy Dolovov <Dmitriy.Dolovov@jetbrains.com>
2021-09-30 11:47:43 +0300

    wip: my enhanced DFS

--- native/tests-blackbox/tests/org/jetbrains/kotlin/konan/blackboxtest/Utils.kt
+++ native/tests-blackbox/tests/org/jetbrains/kotlin/konan/blackboxtest/Utils.kt
@@ -8,6 +8,7 @@ package org.jetbrains.kotlin.konan.blackboxtest
 import org.jetbrains.kotlin.renderer.KeywordStringsGenerated.KEYWORDS
 import org.jetbrains.kotlin.test.services.JUnit5Assertions.assertTrue
 import org.jetbrains.kotlin.test.util.KtTestUtil
+import org.jetbrains.kotlin.utils.DFS
 import java.io.File
 import java.nio.charset.Charset
 import java.nio.file.Path
@@ -68,3 +69,49 @@ internal val Class<*>.sanitizedName: String
 internal const val DEFAULT_FILE_NAME = "main.kt"
 internal const val DEFAULT_MODULE_NAME = "default"
 internal const val SUPPORT_MODULE_NAME = "support"
+
+internal fun Set<PackageName>.findCommonPackageName(): PackageName =
+    map { packageName: PackageName ->
+        packageName.split('.')
+    }.reduce { commonPackageNameParts: List<String>, packageNameParts: List<String> ->
+        mutableListOf<String>().apply {
+            val i = commonPackageNameParts.iterator()
+            val j = packageNameParts.iterator()
+
+            while (i.hasNext() && j.hasNext()) {
+                val packageNamePart = i.next()
+                if (packageNamePart == j.next()) add(packageNamePart) else break
+            }
+        }
+    }.joinToString(".")
+
+internal object DFSEx {
+    fun <N : Any> reverseTopologicalOrderWithoutCycles(
+        nodes: Iterable<N>,
+        getNeighbors: (N) -> Iterable<N>,
+        onCycleDetected: (Iterable<N>) -> Nothing
+    ): List<N> {
+        val visitedNodes = linkedMapOf<N, State>()
+
+        val visitHandler = DFS.Visited<N> { true }
+        val nodeHandler = object : DFS.AbstractNodeHandler<N, Nothing>() {
+            override fun beforeChildren(current: N) = when (visitedNodes[current]) {
+                null -> {
+                    visitedNodes[current] = State.VISITING
+                    true
+                }
+                State.VISITED -> false
+                State.VISITING -> onCycleDetected(visitedNodes.keys.dropWhile { it != current })
+            }
+
+            override fun afterChildren(current: N) = check(visitedNodes.put(current, State.VISITED) == State.VISITING)
+            override fun result() = error("Not supposed to be called")
+        }
+
+        nodes.forEach { node -> DFS.doDfs(node, getNeighbors, visitHandler, nodeHandler) }
+
+        return visitedNodes.keys.reversed()
+    }
+
+    private enum class State { VISITING, VISITED }
+}
