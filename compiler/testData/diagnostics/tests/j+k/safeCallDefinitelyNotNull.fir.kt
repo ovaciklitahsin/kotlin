77c034e56a53a611e6888943552bc3160e37e251
Denis.Zharkov <denis.zharkov@jetbrains.com>
2021-10-22 19:59:09 +0300

    FIR: Fix safe call receiver nullability after null check

---
+++ compiler/testData/diagnostics/tests/j+k/safeCallDefinitelyNotNull.fir.kt
@@ -0,0 +1,10 @@
+// FILE: Api.java
+import org.jetbrains.annotations.NotNull;
+public abstract class Api<T> {
+    public abstract void typeOf(@NotNull T node);
+}
+
+// FILE: main.kt
+fun <E> foo(a: Api<E>, e: E?) {
+    e?.let { a.typeOf(e) }
+}
