3b44d3841c6e1d76b336c908400ab5532f7b65f6
Artem Kobzar <artem.kobzar@jetbrains.com>
2021-11-02 17:55:30 +0200

    fix(d.ts): remove unused block comments after exported implemented interfaces list.

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/export/ExportModelToTsDeclarations.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/export/ExportModelToTsDeclarations.kt
@@ -113,31 +113,9 @@ fun ExportedDeclaration.toTypeScript(indent: String, prefix: String = ""): Strin
     is ExportedClass -> {
         val keyword = if (isInterface) "interface" else "class"
         val superInterfacesKeyword = if (isInterface) "extends" else "implements"
-        val (exportedInterfaces, nonExportedInterfaces) = superInterfaces.partition { it !is ExportedType.ImplicitlyExportedType }
 
-        val superClassClause = superClass?.let {
-            if (it is ExportedType.ImplicitlyExportedType) {
-                " /* extends ${it.type.toTypeScript(indent)} */"
-            } else {
-                " extends ${it.toTypeScript(indent)}"
-            }
-        } ?: ""
-        val superInterfacesClause = when {
-            exportedInterfaces.isEmpty() && nonExportedInterfaces.isNotEmpty() ->
-                " /* $superInterfacesKeyword ${
-                    nonExportedInterfaces.joinToString(", ") {
-                        (it as ExportedType.ImplicitlyExportedType).type.toTypeScript(indent)
-                    }
-                } */"
-            exportedInterfaces.isNotEmpty() ->
-                " $superInterfacesKeyword " + exportedInterfaces.joinToString(", ") { it.toTypeScript(indent) } +
-                        "/*, ${
-                            nonExportedInterfaces.joinToString(", ") {
-                                (it as ExportedType.ImplicitlyExportedType).type.toTypeScript(indent)
-                            }
-                        } */"
-            else -> ""
-        }
+        val superClassClause = superClass?.let { it.toExtendsClause(indent) } ?: ""
+        val superInterfacesClause = superInterfaces.toImplementsClause(superInterfacesKeyword, indent)
 
         val members = members.map {
             if (!ir.isInner || it !is ExportedFunction || !it.isStatic) {
@@ -175,6 +153,30 @@ fun ExportedDeclaration.toTypeScript(indent: String, prefix: String = ""): Strin
             if (nestedClasses.isNotEmpty()) "\n" + ExportedNamespace(name, nestedClasses).toTypeScript(indent, prefix) else ""
 
         if (name.isValidES5Identifier()) klassExport + staticsExport else ""
+    }
+}
+
+fun ExportedType.toExtendsClause(indent: String): String {
+    return if (this is ExportedType.ImplicitlyExportedType) {
+        " /* extends ${type.toTypeScript(indent)} */"
+    } else {
+        " extends ${toTypeScript(indent)}"
+    }
+}
+
+fun List<ExportedType>.toImplementsClause(superInterfacesKeyword: String, indent: String): String {
+    val (exportedInterfaces, nonExportedInterfaces) = partition { it !is ExportedType.ImplicitlyExportedType }
+    val listOfNonExportedInterfaces = nonExportedInterfaces.joinToString(", ") {
+        (it as ExportedType.ImplicitlyExportedType).type.toTypeScript(indent)
+    }
+    return when {
+        exportedInterfaces.isEmpty() && nonExportedInterfaces.isNotEmpty() ->
+            " /* $superInterfacesKeyword $listOfNonExportedInterfaces */"
+        exportedInterfaces.isNotEmpty() -> {
+            val nonExportedInterfacesTsString = if (nonExportedInterfaces.isNotEmpty()) "/*, $listOfNonExportedInterfaces */" else ""
+            " $superInterfacesKeyword " + exportedInterfaces.joinToString(", ") { it.toTypeScript(indent) } + nonExportedInterfacesTsString
+        }
+        else -> ""
     }
 }
 
